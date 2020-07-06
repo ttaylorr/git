0d12a4d7bc82d5cb0d7588d88b078d2c5b2cead7
Taylor Blau <me@ttaylorr.com>
2020-07-06 09:53:05 -0700

    commit-graph: introduce 'get_bloom_filter_settings()'
    Many places in the code often need a pointer to the commit-graph's
    'struct bloom_filter_settings', in which case they often take the value
    from the top-most commit-graph.
    
    In the non-split case, this works as expected. In the split case,
    however, things get a little tricky. Not all layers in a chain of
    incremental commit-graphs are required to themselves have Bloom data,
    and so whether or not some part of the code uses Bloom filters depends
    entirely on whether or not the top-most level of the commit-graph chain
    has Bloom filters.
    
    This has been the behavior since Bloom filters were introduced, and has
    been codified into the tests since a759bfa9ee (t4216: add end to end
    tests for git log with Bloom filters, 2020-04-06). In fact, t4216.130
    requires that Bloom filters are not used in exactly the case described
    earlier.
    
    There is no reason that this needs to be the case, since it is perfectly
    valid for commits in an earlier layer to have Bloom filters when commits
    in a newer layer do not.
    
    Since Bloom settings are guaranteed to be the same for any layer in a
    chain that has Bloom data, it is sufficient to traverse the
    '->base_graph' pointer until either (1) a non-null 'struct
    bloom_filter_settings *' is found, or (2) until we are at the root of
    the commit-graph chain.
    
    Introduce a 'get_bloom_filter_settings()' function that does just this,
    and use it instead of purely dereferencing the top-most graph's
    '->bloom_filter_settings' pointer.
    
    Co-authored-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- revision.c
+++ revision.c
@@ -684,7 +684,7 @@ static void prepare_to_use_bloom_filter(struct rev_info *revs)
 	if (!revs->repo->objects->commit_graph)
 		return;
 
-	revs->bloom_filter_settings = revs->repo->objects->commit_graph->bloom_filter_settings;
+	revs->bloom_filter_settings = get_bloom_filter_settings(revs->repo);
 	if (!revs->bloom_filter_settings)
 		return;
 
