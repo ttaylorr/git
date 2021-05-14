c9d6281b19d6a239dc1c255b74155d9853bef3bb
Taylor Blau <me@ttaylorr.com>
2021-05-14 11:20:13 -0400

    builtin/pack-objects.c: support creating cruft packs
    In the next patch, `git repack` will gain a new `--cruft` option,
    allowing it to pack unreachable objects separately. But the majority of
    the work to create a cruft pack will be done by `pack-objects` using
    what is implemented in this patch.
    
    `git pack-objects --cruft` creates a new pack (and an `.mtimes` file)
    containing objects that aren't found in any `--keep-pack`. To create a
    cruft pack `git repack` will pass the names of any packs which are not
    getting deleted as `--keep-pack`s. Since `git repack` usually only
    creates packs which contain reachable objects, objects that aren't
    contained in those packs are known to be unreachable.
    
    Accumulating cruft objects works roughly as follows:
    
      - Add every non-expired loose and packed object not contained in a
        kept pack to a traversal.
    
      - Run that traversal, adding objects that we encounter along the way
        to the new cruft pack, and stopping once we hit an object contained
        in any kept pack(s).
    
    The timestamp of an object is determined using the following rules:
    
      1. If an object is found loose, then its timestamp is the mtime of the
         loose object file.
    
      2. If an object is found in a cruft pack, then its timestamp can be
         read out of the corresponding `.mtimes` file.
    
      3. Otherwise, the object belongs to a non-cruft pack, in which case
         its timestamp is the mtime of that pack.
    
    Why is the reachability traversal necessary? It is done in order to
    "rescue" cruft objects which would have otherwise been expired (when
    given a finite `--cruft-expiration`). Consider an example where a commit
    (and the trees and blobs reachable from it) is about to be expired, but
    the commit is freshened. In this case we want to avoid pruning the trees
    and blobs even though they may have mtimes occurring earlier than the
    grace period, since there is another object from which they are
    reachable which we are not expiring. The traversal provides the
    reachability information necessary to discover which would-be
    expire-able objects are rescued by non-expired ones.
    
    Confusingly, objects do not propagate their mtimes to objects that they
    can reach. Doing so is tricky, but luckily for us, it is also
    unnecessary, since non-expired objects always rescue any reachable ones.
    That's because we include all objects visited along a traversal
    (including expired ones, which are only discarded when considering
    whether or not to add a tip to the traversal).
    
    Finally, stopping early once an object is found in a kept pack is safe
    to do because the kept packs ordinarily represent which packs will
    survive after repacking. Assuming that it _isn't_ safe to halt a
    traversal early would mean that there is some ancestor object which is
    missing, which implies repository corruption (i.e., the complete set of
    reachable objects isn't present).
    
    (cherry picked from commit 2f14c0b523f166d1b89d3648a0aea7ebe54152e3)

---
+++ t/t5327-pack-objects-cruft.sh
@@ -0,0 +1,295 @@
+#!/bin/sh
+
+test_description='cruft pack related pack-objects tests'
+. ./test-lib.sh
+
+objdir=.git/objects
+packdir=$objdir/pack
+
+test_expect_success 'unreachable loose objects are packed' '
+	test_commit base &&
+	git repack -Ad &&
+	test_commit loose &&
+
+	test-tool chmtime +2000 "$objdir/$(test_oid_to_path \
+		$(git rev-parse loose:loose.t))" &&
+	test-tool chmtime +1000 "$objdir/$(test_oid_to_path \
+		$(git rev-parse loose^{tree}))" &&
+
+	(
+		git rev-list --objects --no-object-names base..loose |
+		while read oid
+		do
+			path="$objdir/$(test_oid_to_path "$oid")" &&
+			printf "%s %d\n" "$oid" "$(test-tool chmtime --get "$path")"
+		done |
+		sort -k1
+	) >expect &&
+
+	keep="$(basename "$(ls $packdir/pack-*.pack)")" &&
+	cruft="$(echo $keep | git pack-objects --cruft $packdir/pack)" &&
+	test-tool pack-mtimes "pack-$cruft.mtimes" >actual &&
+
+	test_cmp expect actual
+'
+
+test_expect_success 'unreachable packed objects are packed' '
+	git init repo &&
+	test_when_finished "rm -fr repo" &&
+	(
+		cd repo &&
+
+		test_commit packed &&
+		git repack -Ad &&
+		test_commit other &&
+
+		git rev-list --objects --no-object-names packed.. >objects &&
+		keep="$(basename "$(ls $packdir/pack-*.pack)")" &&
+		other="$(git pack-objects --delta-base-offset \
+			$packdir/pack <objects)" &&
+		git prune-packed &&
+
+		test-tool chmtime --get -100 "$packdir/pack-$other.pack" >expect &&
+
+		cruft="$(git pack-objects --cruft $packdir/pack <<-EOF
+		$keep
+		-pack-$other.pack
+		EOF
+		)" &&
+		test-tool pack-mtimes "pack-$cruft.mtimes" >actual.raw &&
+
+		cut -d" " -f2 <actual.raw | sort -u >actual &&
+
+		test_cmp expect actual
+	)
+'
+
+test_expect_success 'unreachable cruft objects are repacked' '
+	git init repo &&
+	test_when_finished "rm -fr repo" &&
+	(
+		cd repo &&
+
+		test_commit packed &&
+		git repack -Ad &&
+		test_commit other &&
+
+		git rev-list --objects --no-object-names packed.. >objects &&
+		keep="$(basename "$(ls $packdir/pack-*.pack)")" &&
+
+		cruft_a="$(echo $keep | git pack-objects --cruft $packdir/pack)" &&
+		git prune-packed &&
+		cruft_b="$(git pack-objects --cruft $packdir/pack <<-EOF
+		$keep
+		-pack-$cruft_a.pack
+		EOF
+		)" &&
+
+		test-tool pack-mtimes "pack-$cruft_a.mtimes" >expect.raw &&
+		test-tool pack-mtimes "pack-$cruft_b.mtimes" >actual.raw &&
+
+		sort <expect.raw >expect &&
+		sort <actual.raw >actual &&
+
+		test_cmp expect actual
+	)
+'
+
+test_expect_success 'cruft tags rescue tagged objects' '
+	git init repo &&
+	test_when_finished "rm -fr repo" &&
+	(
+		cd repo &&
+
+		test_commit packed &&
+		git repack -Ad &&
+
+		test_commit tagged &&
+		git tag -a annotated -m tag &&
+
+		git rev-list --objects --no-object-names packed.. >objects &&
+		while read oid
+		do
+			test-tool chmtime -1000 \
+				"$objdir/$(test_oid_to_path $oid)"
+		done <objects &&
+
+		test-tool chmtime -500 \
+			"$objdir/$(test_oid_to_path $(git rev-parse annotated))" &&
+
+		keep="$(basename "$(ls $packdir/pack-*.pack)")" &&
+		cruft="$(echo $keep | git pack-objects --cruft \
+			--cruft-expiration=750.seconds.ago \
+			$packdir/pack)" &&
+		test-tool pack-mtimes "pack-$cruft.mtimes" >actual.raw &&
+		cut -f1 -d" " <actual.raw | sort >actual &&
+
+		(
+			cat objects &&
+			git rev-parse annotated
+		) >expect.raw &&
+		sort <expect.raw >expect &&
+
+		test_cmp expect actual &&
+		cat actual
+	)
+'
+
+test_expect_success 'cruft commits rescue parents, trees' '
+	git init repo &&
+	test_when_finished "rm -fr repo" &&
+	(
+		cd repo &&
+
+		test_commit packed &&
+		git repack -Ad &&
+
+		test_commit old &&
+		test_commit new &&
+
+		git rev-list --objects --no-object-names packed..new >objects &&
+		while read object
+		do
+			test-tool chmtime -1000 \
+				"$objdir/$(test_oid_to_path $object)"
+		done <objects &&
+		test-tool chmtime +500 "$objdir/$(test_oid_to_path \
+			$(git rev-parse HEAD))" &&
+
+		keep="$(basename "$(ls $packdir/pack-*.pack)")" &&
+		cruft="$(echo $keep | git pack-objects --cruft \
+			--cruft-expiration=750.seconds.ago \
+			$packdir/pack)" &&
+		test-tool pack-mtimes "pack-$cruft.mtimes" >actual.raw &&
+
+		cut -d" " -f1 <actual.raw | sort >actual &&
+		sort <objects >expect &&
+
+		test_cmp expect actual
+	)
+'
+
+test_expect_success 'cruft trees rescue sub-trees, blobs' '
+	git init repo &&
+	test_when_finished "rm -fr repo" &&
+	(
+		cd repo &&
+
+		test_commit packed &&
+		git repack -Ad &&
+
+		mkdir -p dir/sub &&
+		echo foo >foo &&
+		echo bar >dir/bar &&
+		echo baz >dir/sub/baz &&
+
+		test_tick &&
+		git add . &&
+		git commit -m "pruned" &&
+
+		test-tool chmtime -1000 "$objdir/$(test_oid_to_path $(git rev-parse HEAD))" &&
+		test-tool chmtime -1000 "$objdir/$(test_oid_to_path $(git rev-parse HEAD^{tree}))" &&
+		test-tool chmtime -1000 "$objdir/$(test_oid_to_path $(git rev-parse HEAD:foo))" &&
+		test-tool chmtime  -500 "$objdir/$(test_oid_to_path $(git rev-parse HEAD:dir))" &&
+		test-tool chmtime -1000 "$objdir/$(test_oid_to_path $(git rev-parse HEAD:dir/bar))" &&
+		test-tool chmtime -1000 "$objdir/$(test_oid_to_path $(git rev-parse HEAD:dir/sub))" &&
+		test-tool chmtime -1000 "$objdir/$(test_oid_to_path $(git rev-parse HEAD:dir/sub/baz))" &&
+
+		keep="$(basename "$(ls $packdir/pack-*.pack)")" &&
+		cruft="$(echo $keep | git pack-objects --cruft \
+			--cruft-expiration=750.seconds.ago \
+			$packdir/pack)" &&
+		test-tool pack-mtimes "pack-$cruft.mtimes" >actual.raw &&
+		cut -f1 -d" " <actual.raw | sort >actual &&
+
+		git rev-parse HEAD:dir HEAD:dir/bar HEAD:dir/sub HEAD:dir/sub/baz >expect.raw &&
+		sort <expect.raw >expect &&
+
+		test_cmp expect actual
+	)
+'
+
+test_expect_success 'expired objects are pruned' '
+	git init repo &&
+	test_when_finished "rm -fr repo" &&
+	(
+		cd repo &&
+
+		test_commit packed &&
+		git repack -Ad &&
+
+		test_commit pruned &&
+
+		git rev-list --objects --no-object-names packed..pruned >objects &&
+		while read object
+		do
+			test-tool chmtime -1000 \
+				"$objdir/$(test_oid_to_path $object)"
+		done <objects &&
+
+		keep="$(basename "$(ls $packdir/pack-*.pack)")" &&
+		cruft="$(echo $keep | git pack-objects --cruft \
+			--cruft-expiration=750.seconds.ago \
+			$packdir/pack)" &&
+
+		test-tool pack-mtimes "pack-$cruft.mtimes" >actual &&
+		test_must_be_empty actual
+	)
+'
+
+test_expect_success 'multiple cruft packs' '
+	git init repo &&
+	test_when_finished "rm -fr repo" &&
+	(
+		cd repo &&
+
+		test_commit reachable &&
+		git repack -Ad &&
+		keep="$(basename "$(ls $packdir/pack-*.pack)")" &&
+
+		test_commit cruft &&
+		loose="$objdir/$(test_oid_to_path $(git rev-parse cruft))" &&
+
+		# generate three copies of the cruft object in different
+		# cruft packs, each with a unique mtime:
+		#   - one expired (1000 seconds ago)
+		#   - two non-expired (one 1000 seconds in the future,
+		#     one 1500 seconds in the future)
+		test-tool chmtime =-1000 "$loose" &&
+		git pack-objects --cruft $packdir/pack-A <<-EOF &&
+		$keep
+		EOF
+		test-tool chmtime =+1000 "$loose" &&
+		git pack-objects --cruft $packdir/pack-B <<-EOF &&
+		$keep
+		-$(basename $(ls $packdir/pack-A-*.pack))
+		EOF
+		test-tool chmtime =+1500 "$loose" &&
+		git pack-objects --cruft $packdir/pack-C <<-EOF &&
+		$keep
+		-$(basename $(ls $packdir/pack-A-*.pack))
+		-$(basename $(ls $packdir/pack-B-*.pack))
+		EOF
+
+		# ensure the resulting cruft pack takes the most recent
+		# mtime among all copies
+		cruft="$(git pack-objects --cruft \
+			--cruft-expiration=500.seconds.ago \
+			$packdir/pack <<-EOF
+		$keep
+		-$(basename $(ls $packdir/pack-A-*.pack))
+		-$(basename $(ls $packdir/pack-B-*.pack))
+		-$(basename $(ls $packdir/pack-C-*.pack))
+		EOF
+		)" &&
+
+		test-tool pack-mtimes "$(basename $(ls $packdir/pack-C-*.mtimes))" >expect.raw &&
+		test-tool pack-mtimes "pack-$cruft.mtimes" >actual.raw &&
+
+		sort expect.raw >expect &&
+		sort actual.raw >actual &&
+		test_cmp expect actual
+	)
+'
+
+test_done
