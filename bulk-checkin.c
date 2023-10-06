cb0f79cabb7921ab7e334ad8a467ae84853bbd39
Taylor Blau <me@ttaylorr.com>
2023-10-06 12:55:18 -0400

    bulk-checkin: introduce `index_tree_bulk_checkin_incore()`
    The remaining missing piece in order to teach the `merge-tree` builtin
    how to write the contents of a merge into a pack is a function to index
    tree objects into a bulk-checkin pack.
    
    This patch implements that missing piece, which is a thin wrapper around
    all of the functionality introduced in previous commits.
    
    If and when Git gains support for a "compatibility" hash algorithm, the
    changes to support that here will be minimal. The bulk-checkin machinery
    will need to convert the incoming tree to compute its length under the
    compatibility hash, necessary to reconstruct its header. With that
    information (and the converted contents of the tree), the bulk-checkin
    machinery will have enough to keep track of the converted object's hash
    in order to update the compatibility mapping.
    
    Within `deflate_tree_to_pack_incore()`, the changes should be limited
    to something like:
    
        if (the_repository->compat_hash_algo) {
          struct strbuf converted = STRBUF_INIT;
          if (convert_object_file(&compat_obj,
                                  the_repository->hash_algo,
                                  the_repository->compat_hash_algo, ...) < 0)
            die(...);
    
          format_object_header_hash(the_repository->compat_hash_algo,
                                    OBJ_TREE, size);
    
          strbuf_release(&converted);
        }
    
    , assuming related changes throughout the rest of the bulk-checkin
    machinery necessary to update the hash of the converted object, which
    are likewise minimal in size.
    
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- bulk-checkin.c
+++ bulk-checkin.c
@@ -418,6 +418,20 @@ static int deflate_blob_to_pack_incore(struct bulk_checkin_packfile *state,
 						   flags);
 }
 
+static int deflate_tree_to_pack_incore(struct bulk_checkin_packfile *state,
+				       struct object_id *result_oid,
+				       const void *buf, size_t size,
+				       const char *path, unsigned flags)
+{
+	git_hash_ctx ctx;
+
+	format_object_header_hash(the_hash_algo, &ctx, OBJ_TREE, size);
+
+	return deflate_obj_contents_to_pack_incore(state, &ctx, result_oid,
+						   buf, size, OBJ_TREE, path,
+						   flags);
+}
+
 static int deflate_blob_to_pack(struct bulk_checkin_packfile *state,
 				struct object_id *result_oid,
 				int fd, size_t size,
@@ -502,6 +516,17 @@ int index_blob_bulk_checkin_incore(struct object_id *oid,
 				   const char *path, unsigned flags)
 {
 	int status = deflate_blob_to_pack_incore(&bulk_checkin_packfile, oid,
+						 buf, size, path, flags);
+	if (!odb_transaction_nesting)
+		flush_bulk_checkin_packfile(&bulk_checkin_packfile);
+	return status;
+}
+
+int index_tree_bulk_checkin_incore(struct object_id *oid,
+				   const void *buf, size_t size,
+				   const char *path, unsigned flags)
+{
+	int status = deflate_tree_to_pack_incore(&bulk_checkin_packfile, oid,
 						 buf, size, path, flags);
 	if (!odb_transaction_nesting)
 		flush_bulk_checkin_packfile(&bulk_checkin_packfile);
