14aaf912647c947e65562f09a2fb04a39380e019
Taylor Blau <me@ttaylorr.com>
2025-10-28 11:07:26 -0400

    WIP

--- midx-write.c
+++ midx-write.c
@@ -933,7 +933,8 @@ static int fill_packs_from_midx(struct write_midx_context *ctx)
 	for (m = ctx->m; m; m = m->base_midx) {
 		uint32_t i;
 
-		for (i = 0; i < m->num_packs; i++) {
+		for (i = m->num_packs_in_base;
+		     i < m->num_packs_in_base + m->num_packs; i++) {
 			ALLOC_GROW(ctx->info, ctx->nr + 1, ctx->alloc);
 
 			if (fill_pack_from_midx(&ctx->info[ctx->nr++], m,
